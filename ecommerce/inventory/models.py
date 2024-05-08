5c4f31f2ad6628428995f9aa0e7e404157b70e60
aghee <agymunzala@gmail.com>
2024-05-08 22:31:34 +0300

    add data inlines,nested inlines

--- ecommerce/inventory/models.py
+++ ecommerce/inventory/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 import uuid
+from django.utils.text import slugify 
 
 # Create your models here.
 class Product(models.Model):
@@ -15,7 +16,7 @@ class Product(models.Model):
 
     pid=models.CharField(max_length=200)
     name=models.CharField(max_length=100,unique=True)
-    slug=models.SlugField(null=False,unique=True)
+    slug=models.SlugField(unique=True,blank=True,null=True)
     description=models.TextField(null=True)
     is_digital=models.BooleanField(default=False)
     created_at=models.DateTimeField(auto_now_add=True,editable=False)
@@ -24,11 +25,20 @@ class Product(models.Model):
     stock_status=models.CharField(max_length=3, 
                                   choices=STOCK_STATUS, 
                                   default=OUT_OF_STOCK)
+    # Current_Stock_Count=models.IntegerField(default=0)
     category=models.ForeignKey("Category",on_delete=models.SET_NULL,null=True)
-    seasonal_event=models.ForeignKey("SeasonalEvents",on_delete=models.SET_NULL,null=True)
+    seasonal_event=models.ForeignKey("SeasonalEvent",on_delete=models.SET_NULL,null=True,blank=True)
 
     #field not created, used to create association/link/through  table
-    product_type=models.ManyToManyField("ProductType",related_name="product_type")
+    product_type=models.ManyToManyField("ProductType",related_name="product_type",blank=True)
+
+    def save(self,*args,**kwargs):
+        if not self.slug:
+            self.slug=slugify(self.name)
+        super().save(*args,**kwargs)
+    
+    def __str__(self):
+        return self.name
 
 class ProductLine(models.Model):
     price=models.DecimalField(decimal_places=2,max_digits=10000)
@@ -40,37 +50,63 @@ class ProductLine(models.Model):
     product=models.ForeignKey(Product,on_delete=models.PROTECT)
 
     #field not created, used to create a link/through/association table
-    attribute_values=models.ManyToManyField("AttributeValue",related_name="attribute_values") 
+    attribute_values=models.ManyToManyField("AttributeValue",related_name="attribute_values")
+
 
 class ProductImage(models.Model):
-    name=models.CharField(max_length=100)
-    alternative_text=models.CharField(max_length=100)
+    # name=models.CharField(max_length=100)
+    alternative_text=models.CharField(max_length=200)
     url=models.ImageField()
     order=models.IntegerField()
     product_line=models.ForeignKey(ProductLine,on_delete=models.CASCADE)
 
 class Category(models.Model):
-    name=models.CharField(max_length=100,unique=True)
-    slug=models.SlugField(unique=True)
+    name=models.CharField(max_length=100,unique=True,verbose_name="Item name",help_text="Enter a category")
+    slug=models.SlugField(unique=True,null=True,blank=True)
     is_active=models.BooleanField(default=False)
-    parent=models.ForeignKey("self",on_delete=models.PROTECT) #selfreferencing FK
+    parent=models.ForeignKey("self",on_delete=models.PROTECT,null=True,blank=True) #selfreferencing FK
 
-class SeasonalEvents(models.Model):
+    class Meta:
+        verbose_name="Inventory Category"
+        verbose_name_plural="Categories"
+    
+    def save(self,*args,**kwargs):
+        if not self.slug:
+            self.slug=slugify(self.name)
+        super().save(*args,**kwargs)
+
+    def __str__(self):
+        return self.name
+
+class SeasonalEvent(models.Model):
+    id=models.BigAutoField(primary_key=True)
     start_date=models.DateTimeField()
     end_date=models.DateTimeField()
     name=models.CharField(max_length=100,unique=True)
+
+    def __str__(self):
+        return self.name
 
 class Attribute(models.Model):
     name=models.CharField(max_length=100)
     description=models.TextField(null=True)
 
+    def __str__(self):
+        return self.name
+
 class ProductType(models.Model):
     name=models.CharField(max_length=100)
-    parent=models.ForeignKey('self',on_delete=models.CASCADE)
+    parent=models.ForeignKey('self',on_delete=models.PROTECT,null=True,blank=True)
+
+    def __str__(self):
+        return self.name
 
 class AttributeValue(models.Model):
     attribute_value=models.CharField(max_length=100)
     attribute=models.ForeignKey(Attribute,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.attribute.name}:{self.attribute_value}"
 
 class ProductLine_AttributeValue(models.Model):
     attribute_value=models.ForeignKey(AttributeValue,on_delete=models.CASCADE)
@@ -79,7 +115,7 @@ class ProductLine_AttributeValue(models.Model):
 class Product_ProductType(models.Model):
     product=models.ForeignKey(Product,on_delete=models.CASCADE)
     product_type=models.ForeignKey(ProductType,on_delete=models.CASCADE)
-
+    
 class StockControl(models.Model):
     stock_quantity=models.IntegerField()
     name=models.CharField(max_length=100)
