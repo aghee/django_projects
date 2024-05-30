ac882bc7741091b15d9a4e6c135d2332f18f2cf7
aghee <agymunzala@gmail.com>
2024-05-30 09:40:55 +0300

    add login, logout using LoginView-CBV, Logout-FBV

--- Task_tracking_app/todo/urls.py
+++ Task_tracking_app/todo/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
-from .views import TaskList,TaskDetail,TaskCreate,TaskUpdate,TaskDelete
+from .views import TaskList,TaskDetail,TaskCreate,TaskUpdate,TaskDelete,CustomLoginView,logoutUser
+# from django.contrib.auth.views import LogoutView
+
 
 urlpatterns=[
     path("",TaskList.as_view(),name="all_tasks"),
+    path("login/",CustomLoginView.as_view(),name="login"),
+    # path("logout/",LogoutView.as_view(next_page="login"),name="logout"),
+    path("logout/",logoutUser,name="logout"),
     path("task/<int:pk>/",TaskDetail.as_view(),name="detailtask"),
     path("task-create/",TaskCreate.as_view(),name="createtask"),
     path("task-update/<int:pk>",TaskUpdate.as_view(),name="updatetask"),
