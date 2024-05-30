7486708510492c6a4c283b3b733172e2ad11ff40
aghee <agymunzala@gmail.com>
2024-05-30 13:11:13 +0300

    Fix:add urlpatterns for password reset

--- Task_tracking_app/todo/urls.py
+++ Task_tracking_app/todo/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from .views import TaskList,TaskDetail,TaskCreate,TaskUpdate,TaskDelete,CustomLoginView,logoutUser,Registration
+from django.contrib.auth import views as auth_views
 # from django.contrib.auth.views import LogoutView
 
 
@@ -8,6 +9,11 @@ urlpatterns=[
     path("login/",CustomLoginView.as_view(),name="login"),
     path("register/",Registration.as_view(),name="register"),
     # path("logout/",LogoutView.as_view(next_page="login"),name="logout"),
+    path("reset_password/",auth_views.PasswordResetView.as_view(template_name="todo/password_reset.html"),name="password_reset"),
+    path("reset_password_sent/",auth_views.PasswordResetDoneView.as_view(template_name="todo/password_reset_sent.html"),name="password_reset_done"),
+    path("reset/<uidb64>/<token>",auth_views.PasswordResetConfirmView.as_view(template_name="todo/password_reset_form.html"),name="password_reset_confirm"),
+    path("reset_password_complete",auth_views.PasswordResetCompleteView.as_view(template_name="todo/password_reset_complete.html"),name="password_reset_complete"),
+
     path("logout/",logoutUser,name="logout"),
     path("task/<int:pk>/",TaskDetail.as_view(),name="detailtask"),
     path("task-create/",TaskCreate.as_view(),name="createtask"),
